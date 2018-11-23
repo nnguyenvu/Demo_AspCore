09aab1af69852a3eae428737b9fff95084e57510
NguyenVu <vuu.nnguyen@gmail.com>
2018-11-24 01:56:36 +0700

    upload folder code

---
+++ ASPCORE_View/View_Demo/Controllers/ProductController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using View_Demo.Models;
+
+namespace View_Demo.Controllers
+{
+    public class ProductController : Controller
+    {
+        public static List<Product> products = new List<Product>()
+        {
+            new Product
+                {
+                    MaHH = 001,
+                    TenHH = "Iphone X",
+                    DonGia = 19890000
+                },
+                new Product
+                {
+                    MaHH = 002,
+                    TenHH = "Samsung S9",
+                    DonGia = 19890000
+                },
+                new Product
+                {
+                    MaHH = 003,
+                    TenHH = "Nokia 6",
+                    DonGia = 19890000
+                }
+        };
+        public IActionResult ShowAll()
+        {
+            //List<Product> list = new List<Product>()
+            //{
+            //    new Product
+            //    {
+            //        MaHH = 001,
+            //        TenHH = "Iphone X",
+            //        DonGia = 19890000
+            //    },
+            //    new Product
+            //    {
+            //        MaHH = 002,
+            //        TenHH = "Samsung S9",
+            //        DonGia = 19890000
+            //    },
+            //    new Product
+            //    {
+            //        MaHH = 003,
+            //        TenHH = "Nokia 6",
+            //        DonGia = 19890000
+            //    },
+            //};
+            return View(products);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            products.Add(product);
+            return RedirectToAction("ShowAll");
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ASPCORE_View/View_Demo/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace View_Demo.Models
+{
+    public class Product
+    {
+        [Display(Name="Mã Hàng Hóa")]
+        public int MaHH { get; set; }
+        [Display(Name = "Tên Hàng Hóa")]
+        public string TenHH { get; set; }
+        [Display(Name = "Đơn giá")]
+        public double DonGia { get; set; }
+
+    }
+}
